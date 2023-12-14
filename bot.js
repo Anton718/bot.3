b6c9525909fd01ef14e3f08e87e2d6f871be708b
Anton <112634784+Anton718@users.noreply.github.com>
2023-12-14 23:29:23 +0000

    add updates

--- bot.js
+++ bot.js
@@ -5,11 +5,22 @@ const bot = new Telegraf(process.env.TOKEN);
 const https = require("https")
 const fs = require("fs")
 const sleep = require("sleep")
-const calendar = require("./controllers/calendar")
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const { key_days } = require("./assets/keyboards")
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = months[currentDate.getMonth()];
+const month_int = new Date().getMonth();
+const day = days[currentDate.getDay()];
+const day_int = currentDate.getDay()
+const monthDays = new Date(year, month_int, 0).getDate();
 
-bot.start(calendar.cal)
 
-bot.hears("hey", calendar.cal)
+bot.hears('hi', async (ctx) => {
+    ctx.sendMessage(`Today: ${month} ${day_int}, ${year}`, key_days)
+})
+
 bot.hears("hi", (ctx) => {
     ctx.reply("hi man")
 })
