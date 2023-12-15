717595e8b50e03fc2a1097db6a86e5fad2adc7fc
Anton718 <anton.kostroma@yahoo.com>
2023-12-15 16:05:37 -0500

    add updates

--- controllers/callbacks.js
+++ controllers/callbacks.js
@@ -22,7 +22,7 @@ exports.callbacks = (ctx) => {
 
     Number(res) !== 0 && res.slice(-2) !== "00"?
     (getDate(res), ctx.reply(`${month}, ${res}`, key_hours)): 
-    res.slice(-2) === "00"? (getTime(res), ctx.reply(`Booked for ${res}`)) : null
+    res.slice(-2) === "00"? getTime(res) : null
 
     function getDate(date) {
         const obj = fs.readFileSync("./assets/users.json", "utf-8")
@@ -47,10 +47,11 @@ exports.callbacks = (ctx) => {
         if (obj) {
             const data = JSON.parse(obj);
             for (let i of data) {
-                if (i.user === appointment.user) {
-                    i.hour = time;
-                }
-                fs.writeFileSync("./assets/users.json", JSON.stringify(data))
+                    if (i.user === appointment.user) {
+                        i.hour = time;
+                        ctx.reply(`Booked successfully! ${month} ${i.date}th at ${i.hour}`)
+                        fs.writeFileSync("./assets/users.json", JSON.stringify(data))
+                    }
             }
         } 
     }
